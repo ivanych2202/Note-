c6bb4aae5f30c9e6de480ff7077ade5cd5939bc7
ivanych2202 <ivanych2202@yandex.ru>
2024-02-19 15:25:10 +0300

    Initial Commit

---
+++ Note+/Note_App.swift
@@ -0,0 +1,17 @@
+//
+//  Note_App.swift
+//  Note+
+//
+//  Created by Ivan Elonov on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Note_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
